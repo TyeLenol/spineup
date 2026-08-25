32adaebd88631240afc5d000b26f6081fc9843b7
TyeLenol <1.70892285e+08+TyeLenol@users.noreply.github.com>
2026-08-25 22:00:18 +0000

    test: avoid platform override in settings test

--- test/settings_screen_test.dart
+++ test/settings_screen_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -6,12 +5,7 @@ import 'package:spineup/screens/settings_screen.dart';
 
 void main() {
   setUp(() {
-    debugDefaultTargetPlatformOverride = TargetPlatform.linux;
     SharedPreferences.setMockInitialValues({});
-  });
-
-  tearDown(() {
-    debugDefaultTargetPlatformOverride = null;
   });
 
   testWidgets(
